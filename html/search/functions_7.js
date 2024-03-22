a484c8ac9fa6e152fa0d8181ab45f01c6ec8b098
Maksim Piriyev <maksimpiriyev@gmail.com>
2024-03-22 23:00:09 +0400

    #doc update

--- html/search/functions_7.js
+++ html/search/functions_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['reserve_0',['reserve',['../class_hall.html#a119373fa3e6dd5ec978a93fdceab2b15',1,'Hall']]],
-  ['runserver_1',['RunServer',['../_booking_server_8cpp.html#ace67894655a6679bff0669dc836ddd9b',1,'RunServer(int port):&#160;BookingServer.cpp'],['../_booking_server_8h.html#a1549377e2683da816a2e4814666591cd',1,'RunServer(int port=9001):&#160;BookingServer.cpp']]]
+  ['runserver_1',['RunServer',['../_booking_server_8cpp.html#ace67894655a6679bff0669dc836ddd9b',1,'RunServer(int port):&#160;BookingServer.cpp'],['../_booking_server_8h.html#a6e61f7117525d5c267186ec83fe450e6',1,'RunServer(int port=8080):&#160;BookingServer.cpp']]]
 ];
