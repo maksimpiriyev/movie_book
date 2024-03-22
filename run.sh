74a909a732fe222032e4993d000510f291d09055
Maksim Piriyev <maksimpiriyev@gmail.com>
2024-03-22 23:52:51 +0400

    #run.sh rebase fix

--- run.sh
+++ run.sh
@@ -11,22 +11,22 @@ echo "List of movies"
 curl --location 'localhost:8080/list/movies'
 
 echo "List of theatres that shows movie: 1"
-curl --location 'localhost:9001/list/theatres/1'
+curl --location 'localhost:8080/list/theatres/1'
 
 echo "List of empty seats that show movie:1 in theatre:1"
-curl --location 'localhost:9001/list/theatre-seats/1/1'
+curl --location 'localhost:8080/list/theatre-seats/1/1'
 
 echo "List of bookings@0"
-curl --location 'localhost:9001/list/bookings'
+curl --location 'localhost:8080/list/bookings'
 
 echo "Book seats 3 and 4 for movie 1 in theatre 1"
-curl --location 'localhost:9001/book' --header 'Content-Type: application/json' --data '{"theatre_id": 1, "movie_id": 1, "seats" : [3 , 4]}'
+curl --location 'localhost:8080/book' --header 'Content-Type: application/json' --data '{"theatre_id": 1, "movie_id": 1, "seats" : [3 , 4]}'
 
 echo "List of bookings@1"
-curl --location 'localhost:9001/list/bookings'
+curl --location 'localhost:8080/list/bookings'
 
 echo "Try to book seats 3 and 4 for movie 1 in theatre 1 one more time "
-curl --location 'localhost:9001/book' --header 'Content-Type: application/json' --data '{"theatre_id": 1, "movie_id": 1, "seats" : [3 , 4]}'
+curl --location 'localhost:8080/book' --header 'Content-Type: application/json' --data '{"theatre_id": 1, "movie_id": 1, "seats" : [3 , 4]}'
 
 echo "List of bookings@2"
-curl --location 'localhost:9001/list/bookings'
+curl --location 'localhost:8080/list/bookings'
